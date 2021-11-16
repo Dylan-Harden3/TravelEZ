42231f20d0083d13a445448f6a8c2713cbfcadea
dylanharden3 <dylanharden3@tamu.edu>
2021-11-16 09:58:58 -0600

    changed port

--- index.js
+++ index.js
@@ -568,6 +568,6 @@ app.get('/getresults/:search', async (req,res) => {
     }
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('listening 3000')
 });
